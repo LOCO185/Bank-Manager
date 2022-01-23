e9f35c0eee35c9e1c0ec8debf64f38c47a94d084
LOCO185 <jloco151@gmail.com>
2022-01-23 10:28:27 +0200

    fixed files

--- backend/src/db/mongoose.js
+++ backend/src/db/mongoose.js
@@ -1,4 +1,2 @@
 const mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb+srv://BankApi:jawad123@cluster0.pbqim.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-);
+mongoose.connect(process.env.MONGO_URL);
