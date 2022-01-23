666b12e13ec66fa0f69ae760dd9c6e04644f7db2
LOCO185 <jloco151@gmail.com>
2022-01-23 13:05:54 +0200

    fixed dotenv

--- backend/src/db/mongoose.js
+++ backend/src/db/mongoose.js
@@ -1,2 +1,4 @@
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGO_URL);
+require("dotenv").config();
+const PAS = process.env.PAS;
+mongoose.connect(`mongodb+srv://BankApi:${PAS}@cluster0.pbqim.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
